cc8238d6a9b5d321eec4d93c3a67d053b2e59797
Daniel Brunner <0xFEEDC0DE64@gmail.com>
2016-11-23 18:42:22 +0100

    Initial commit

---
+++ mobile/src/main/java/com/example/daniel/myapplication/fragments/ManageFragment.java
@@ -0,0 +1,20 @@
+package com.example.daniel.myapplication.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.daniel.myapplication.R;
+
+public class ManageFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_manage, container, false);
+
+        return rootView;
+    }
+}
